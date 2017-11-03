f8c35eaa4685bb140aa8b9011ca36783dd6aa7c3
natedaag <natedaage@yahoo.com>
2017-11-03 11:13:55 -0600

    not sure if my eclipse is acting up

--- src/com/fizzbuzz/personal/FizzBuzz.java
+++ src/com/fizzbuzz/personal/FizzBuzz.java
@@ -32,32 +32,41 @@ public class FizzBuzz {
 
 	public static final int DEFAULT_UPPER_LIMIT = 100;
 
-	/**
-	 * Interpret upper limit from command line arguments 
-	 * and invoke emitFizzBuzz method with upper limit.
-	 * 
-	 * @param args
-	 *            Command line arguments
-	 */
 	public static void main(String[] args) {
 		int upperLimit = (args.length > 0) ? Integer.parseInt(args[0]) : DEFAULT_UPPER_LIMIT;
 		emitFizzBuzz(upperLimit);
 	}
 
-	private static void emitFizzBuzz(int limit) {
-		for (int i = 1; i <= limit; i++) {
+//	private static void emitFizzBuzz(int limit) {
+//		for (int i = 1; i <= limit; i++) {
+//			String response = "";
+//			if (i % 3 == 0) {
+//				response += "Fizz";
+//			}
+//			if (i % 5 == 0) {
+//				response += "Buzz";
+//			}
+//			if (response.length() == 0) {// Is response still empty?
+//			response = String.format("%d", i);
+//			}
+//			System.out.println(response);
+//
+//		}
+//	}
+	private static void emitFizzBuzz(int limit){
+		for (int i = 1; i < limit; i++){
 			String response = "";
-			if (i % 3 == 0) {
+			if (i % 3 == 0){
 				response += "Fizz";
 			}
-			if (i % 5 == 0) {
+			if(i % 5 == 0) {
 				response += "Buzz";
 			}
-			if (response.length() == 0) {// Is response still empty?
-			response = String.format("%d", i);
+			if (response.length() == 0) {
+				response += i;
 			}
 			System.out.println(response);
-
 		}
 	}
+	
 }
